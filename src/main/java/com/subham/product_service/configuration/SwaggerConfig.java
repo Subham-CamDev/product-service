33122055043ac6ad83839ea210fc6be08046a1dd
Subham-CamDev <subhambhatta2212@gmail.com>
2026-02-23 11:05:12 +0530

    Added custom Swagger/openAPI config class

---
+++ src/main/java/com/subham/product_service/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.subham.product_service.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .info(new Info()
+                    .title("Product Service API")
+                    .version("1.0")
+                    .description("REST API documentation for the Product Service")
+                    .contact(new Contact()
+                            .name("Subham Microservices Tutorial")
+                            .email("subham@microservices.com")));
+  }
+}
