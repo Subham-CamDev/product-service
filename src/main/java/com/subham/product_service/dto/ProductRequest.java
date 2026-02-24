ea4e210e768f009af69ac267e8cc2449fe6cb082
Subham-CamDev <subhambhatta2212@gmail.com>
2026-02-24 19:58:20 +0530

    Added the sku-code field

--- src/main/java/com/subham/product_service/dto/ProductRequest.java
+++ src/main/java/com/subham/product_service/dto/ProductRequest.java
@@ -2,5 +2,5 @@ package com.subham.product_service.dto;
 
 import java.math.BigDecimal;
 
-public record ProductRequest(String id, String name, String description, BigDecimal price) {
+public record ProductRequest(String id, String skuCode, String name, String description, BigDecimal price) {
 }
